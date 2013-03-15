c36438318e51e556c0b430cf8af0ac6e21afdf7b
delacruz <>
2013-03-15 10:31:15 +0000

    Code for Stops reweighting

---
+++ Stop_TopChi0_Reweighting.C
@@ -0,0 +1,312 @@
+/////// ##### Stop -> top neutralino reweighting and utilitites #####
+/////// J. Alcaraz, B. De La Cruz (CMS, Feb 2013)
+#include "TROOT.h"
+#include "TLorentzVector.h"
+
+struct SUSYGenParticle { // To be filled with status-3 genParticles
+      int pdgId; // PDG identifier (with sign, please)
+      int firstMother; // first mother, set to <0 if no mothers
+      double energy; // energy [GeV]
+      double pt; // pt [GeV]
+      double eta; // eta
+      double phi; // phi
+};
+
+// Get theta effective mixing for a given top polarization hypothesis
+// The result also depends on the stop, top and chi0 mass hypotheses
+// Also valid for an off-shell scenario
+double GetThetaMixing(double topPol, double m_stop, double m_top, double m_chi0) {
+      double p_chi0 = sqrt(pow(m_top*m_top+m_chi0*m_chi0-m_stop*m_stop,2)/4 - pow(m_top*m_chi0,2))/m_stop;
+      double e_chi0 = sqrt(p_chi0*p_chi0+m_chi0*m_chi0);
+      double tanThetaEff = (p_chi0*sqrt(1-topPol*topPol)-m_chi0*topPol)/(topPol*e_chi0+p_chi0);
+      // This is also a valid solution leading to the same top polarization value
+      //double tanThetaEff = (-p_chi0*sqrt(1-topPol*topPol)-m_chi0*topPol)/(topPol*e_chi0+p_chi0);
+      return atan(tanThetaEff);
+}
+
+// Get the top polarization given an effective theta mixing hypothesis
+// The result also depends on the stop, top and chi0 mass hypotheses
+// Also valid for an off-shell scenario
+double GetTopPolarization(double thetaMixing, double m_stop, double m_top, double m_chi0) {
+      double p_chi0 = sqrt(pow(m_top*m_top+m_chi0*m_chi0-m_stop*m_stop,2)/4 - pow(m_top*m_chi0,2))/m_stop;
+      double e_chi0 = sqrt(p_chi0*p_chi0+m_chi0*m_chi0);
+      double pol = p_chi0*cos(2*thetaMixing)/(e_chi0+m_chi0*sin(2*thetaMixing));
+      return pol;
+}
+
+// This function estimates the average polarization in the event
+// for a given thetaMixingEffective scenario. We note that
+// top and (-)antitop polarizations only coincide for narrow stop, top, chi0 masses
+// I.e., the function only makes full sense for an on-shell case with narrow widths
+double AverageTopPolarization_Stop_to_TopChi0(double thetaMixing, std::vector<SUSYGenParticle> genParticles) {
+
+    double pol1 = 0.;
+    double pol2 = 0.;
+
+    unsigned int ngen = genParticles.size();
+
+    for (unsigned int ig=0; ig<ngen; ++ig) {
+      // Find the stop
+      const SUSYGenParticle& genStop = genParticles[ig];
+      if (abs(genStop.pdgId)!=1000006) continue; // find the stop
+      int igstop = ig;
+
+      TLorentzVector stop4;
+      stop4.SetPtEtaPhiE(genStop.pt, genStop.eta, genStop.phi, genStop.energy);
+      TVector3 betaV(-stop4.Px()/stop4.Energy(),-stop4.Py()/stop4.Energy(),-stop4.Pz()/stop4.Energy());
+
+      for (unsigned int ig=igstop+1; ig<ngen; ++ig) { // assume chi0 comes later
+            // Move chi0 to the stop rest frame
+            const SUSYGenParticle& genChi0 = genParticles[ig];
+            if (genChi0.firstMother!=igstop) continue;
+            if (abs(genChi0.pdgId)!=1000022) continue;
+
+            TLorentzVector chi04;
+            chi04.SetPtEtaPhiE(genChi0.pt, genChi0.eta, genChi0.phi, genChi0.energy);
+            chi04.Boost(betaV);
+
+            if (genParticles[igstop].pdgId>0) {
+                  pol1 = chi04.P()*cos(2*thetaMixing)/(chi04.Energy()+chi04.M()*sin(2*thetaMixing));
+            } else {
+                  pol2 = chi04.P()*cos(2*thetaMixing)/(chi04.Energy()+chi04.M()*sin(2*thetaMixing));
+            }
+      
+            break;
+      }
+      
+    }
+
+    return (pol1+pol2)/2;
+}
+
+// The following reweighting only makes sense for on-shell stop, top and chi0
+// In the off-shell case top and anti-top may get very different polarizations
+double Reweight_Stop_to_TopChi0 (std::vector<SUSYGenParticle> genParticles, double referenceTopPolarization, double requestedTopPolarization) {
+    double weight = 1.;
+
+    unsigned int ngen = genParticles.size();
+
+    for (unsigned int ig=0; ig<ngen; ++ig) {
+      const SUSYGenParticle& gen = genParticles[ig];
+      if (gen.firstMother<0) continue;
+      if (abs(gen.pdgId)>20) continue; // expect quarks or leptons from W decay
+
+      // Navigate upwards in the stop->top->W->fermion decay chain
+      const SUSYGenParticle& genW = genParticles[gen.firstMother];
+      if (genW.firstMother<0) continue;
+      if (abs(genW.pdgId)!=24) continue;
+      const SUSYGenParticle& genTop = genParticles[genW.firstMother];
+      if (abs(genTop.pdgId)!=6) continue;
+
+      // We only care about the down-type fermion
+      if (genTop.pdgId*gen.pdgId>0) continue;
+
+      // We also need a stop
+      if (genTop.firstMother<0) continue;
+      const SUSYGenParticle& genStop = genParticles[genTop.firstMother];
+      if (abs(genStop.pdgId)!=1000006) continue;
+
+      // Move top and fermion to the stop center-of-mass frame
+      TLorentzVector stop4;
+      stop4.SetPtEtaPhiE(genStop.pt, genStop.eta, genStop.phi, genStop.energy);
+      TVector3 betaV(-stop4.Px()/stop4.Energy(),-stop4.Py()/stop4.Energy(),-stop4.Pz()/stop4.Energy());
+
+      TLorentzVector top4;
+      top4.SetPtEtaPhiE(genTop.pt, genTop.eta, genTop.phi, genTop.energy);
+      top4.Boost(betaV);
+
+      TLorentzVector ferm4;
+      ferm4.SetPtEtaPhiE(gen.pt, gen.eta, gen.phi, gen.energy);
+      ferm4.Boost(betaV);
+
+      // Do not reweight if by any reason top/fermion directions are undefined
+      // This should be pathological if things are fine
+      if (top4.P()<=0 || ferm4.P()<=0) {
+            printf("Warning: particles at rest, no weight applied: ptop: %.3e, pf: %.3e\n", top4.P(), ferm4.P());
+            continue; 
+      }
+
+      double costh = (top4.Px()*ferm4.Px()+top4.Py()*ferm4.Py()+top4.Pz()*ferm4.Pz())/top4.P()/ferm4.P();
+      
+      double weight_L = (top4.Energy()+top4.P())*(1-costh);
+      double weight_R = (top4.Energy()-top4.P())*(1+costh);
+      weight *= ((1+requestedTopPolarization)*weight_R+(1-requestedTopPolarization)*weight_L)/((1+referenceTopPolarization)*weight_R+(1-referenceTopPolarization)*weight_L);
+
+    }
+
+    return weight;
+
+};
+
+// The following reweighting makes sense in iall cases (off-shell case too)
+// It assumes isotropy for the reference MC and any SUSY scenario as target 
+// (via a thetaMixingTarget input)
+double Reweight_Stop_to_TopChi0_with_SUSYmodel (std::vector<SUSYGenParticle> genParticles, double thetaMixingTarget) { 
+    double weight = 1.;
+
+    unsigned int ngen = genParticles.size();
+
+    for (unsigned int ig=0; ig<ngen; ++ig) {
+      const SUSYGenParticle& gen = genParticles[ig];
+      if (gen.firstMother<0) continue;
+      if (abs(gen.pdgId)>20) continue; // expect quarks or leptons from W decay
+
+      // Navigate upwards in the stop->top->W->fermion decay chain
+      const SUSYGenParticle& genW = genParticles[gen.firstMother];
+      if (genW.firstMother<0) continue;
+      if (abs(genW.pdgId)!=24) continue;
+      const SUSYGenParticle& genTop = genParticles[genW.firstMother];
+      if (abs(genTop.pdgId)!=6) continue;
+
+      // We only care about the down-type fermion
+      if (genTop.pdgId*gen.pdgId>0) continue;
+
+      // We also need a stop
+      if (genTop.firstMother<0) continue;
+      const SUSYGenParticle& genStop = genParticles[genTop.firstMother];
+      if (abs(genStop.pdgId)!=1000006) continue;
+
+      // Move top and fermion to the stop center-of-mass frame
+      TLorentzVector stop4;
+      stop4.SetPtEtaPhiE(genStop.pt, genStop.eta, genStop.phi, genStop.energy);
+      TVector3 betaV(-stop4.Px()/stop4.Energy(),-stop4.Py()/stop4.Energy(),-stop4.Pz()/stop4.Energy());
+
+      int igstop = genTop.firstMother;
+      int igchi0 = -1;
+      TLorentzVector chi04;
+      for (unsigned int ig=igstop+1; ig<ngen; ++ig) { // assume chi0 comes later
+            // Move chi0 to the stop rest frame
+            const SUSYGenParticle& genChi0 = genParticles[ig];
+            if (genChi0.firstMother!=igstop) continue;
+            if (abs(genChi0.pdgId)!=1000022) continue;
+            igchi0 = ig;
+            chi04.SetPtEtaPhiE(genChi0.pt, genChi0.eta, genChi0.phi, genChi0.energy);
+            chi04.Boost(betaV);
+            break;
+      }
+      if (igchi0<0) continue;
+
+      // Determine top polarization in this decay according to model
+      double topPolarization = chi04.P()*cos(2*thetaMixingTarget)/(chi04.Energy()+chi04.M()*sin(2*thetaMixingTarget));
+
+      /*
+      if (genTop.pdgId>0) {
+            printf("Top polarization: %.3f\n", topPolarization);
+      } else {
+            printf("Antitop polarization: %.3f\n", -topPolarization);
+      }
+      */
+      
+      TLorentzVector top4;
+      top4.SetPtEtaPhiE(genTop.pt, genTop.eta, genTop.phi, genTop.energy);
+      top4.Boost(betaV);
+
+      TLorentzVector ferm4;
+      ferm4.SetPtEtaPhiE(gen.pt, gen.eta, gen.phi, gen.energy);
+      ferm4.Boost(betaV);
+
+      // Do not reweight if by any reason top/fermion directions are undefined
+      // This should be pathological if things are fine
+      if (top4.P()<=0 || ferm4.P()<=0) {
+            printf("Warning: particles at rest, no weight applied: ptop: %.3e, pf: %.3e\n", top4.P(), ferm4.P());
+            continue; 
+      }
+
+      double costh = (top4.Px()*ferm4.Px()+top4.Py()*ferm4.Py()+top4.Pz()*ferm4.Pz())/top4.P()/ferm4.P();
+      
+      double weight_L = (top4.Energy()+top4.P())*(1-costh);
+      double weight_R = (top4.Energy()-top4.P())*(1+costh);
+      weight *= ((1+topPolarization)*weight_R+(1-topPolarization)*weight_L)/(weight_R+weight_L);
+
+    }
+
+    return weight;
+
+};
+
+// The following reweighting also makes sense in the off-shell case
+// It goes from a SUSY MC generated with thetaMixingReference to another
+// SUSY MC scenario with thetaMixingTarget
+double Reweight_Stop_to_TopChi0_with_SUSYmodel (std::vector<SUSYGenParticle> genParticles, double thetaMixingReference, double thetaMixingTarget) { 
+    double weight = 1.;
+
+    unsigned int ngen = genParticles.size();
+
+    for (unsigned int ig=0; ig<ngen; ++ig) {
+      const SUSYGenParticle& gen = genParticles[ig];
+      if (gen.firstMother<0) continue;
+      if (abs(gen.pdgId)>20) continue; // expect quarks or leptons from W decay
+
+      // Navigate upwards in the stop->top->W->fermion decay chain
+      const SUSYGenParticle& genW = genParticles[gen.firstMother];
+      if (genW.firstMother<0) continue;
+      if (abs(genW.pdgId)!=24) continue;
+      const SUSYGenParticle& genTop = genParticles[genW.firstMother];
+      if (abs(genTop.pdgId)!=6) continue;
+
+      // We only care about the down-type fermion
+      if (genTop.pdgId*gen.pdgId>0) continue;
+
+      // We also need a stop
+      if (genTop.firstMother<0) continue;
+      const SUSYGenParticle& genStop = genParticles[genTop.firstMother];
+      if (abs(genStop.pdgId)!=1000006) continue;
+
+      // Move top and fermion to the stop center-of-mass frame
+      TLorentzVector stop4;
+      stop4.SetPtEtaPhiE(genStop.pt, genStop.eta, genStop.phi, genStop.energy);
+      TVector3 betaV(-stop4.Px()/stop4.Energy(),-stop4.Py()/stop4.Energy(),-stop4.Pz()/stop4.Energy());
+
+      int igstop = genTop.firstMother;
+      int igchi0 = -1;
+      TLorentzVector chi04;
+      for (unsigned int ig=igstop+1; ig<ngen; ++ig) { // assume chi0 comes later
+            // Move chi0 to the stop rest frame
+            const SUSYGenParticle& genChi0 = genParticles[ig];
+            if (genChi0.firstMother!=igstop) continue;
+            if (abs(genChi0.pdgId)!=1000022) continue;
+            igchi0 = ig;
+            chi04.SetPtEtaPhiE(genChi0.pt, genChi0.eta, genChi0.phi, genChi0.energy);
+            chi04.Boost(betaV);
+            break;
+      }
+      if (igchi0<0) continue;
+
+      // Determine top polarization in this decay according to models
+      double topPolarizationReference = chi04.P()*cos(2*thetaMixingReference)/(chi04.Energy()+chi04.M()*sin(2*thetaMixingReference));
+      double topPolarizationTarget = chi04.P()*cos(2*thetaMixingTarget)/(chi04.Energy()+chi04.M()*sin(2*thetaMixingTarget));
+
+      /*
+      if (genTop.pdgId>0) {
+            printf("Top polarization: %.3f\n", topPolarization);
+      } else {
+            printf("Antitop polarization: %.3f\n", -topPolarization);
+      }
+      */
+      
+      TLorentzVector top4;
+      top4.SetPtEtaPhiE(genTop.pt, genTop.eta, genTop.phi, genTop.energy);
+      top4.Boost(betaV);
+
+      TLorentzVector ferm4;
+      ferm4.SetPtEtaPhiE(gen.pt, gen.eta, gen.phi, gen.energy);
+      ferm4.Boost(betaV);
+
+      // Do not reweight if by any reason top/fermion directions are undefined
+      // This should be pathological if things are fine
+      if (top4.P()<=0 || ferm4.P()<=0) {
+            printf("Warning: particles at rest, no weight applied: ptop: %.3e, pf: %.3e\n", top4.P(), ferm4.P());
+            continue; 
+      }
+
+      double costh = (top4.Px()*ferm4.Px()+top4.Py()*ferm4.Py()+top4.Pz()*ferm4.Pz())/top4.P()/ferm4.P();
+      
+      double weight_L = (top4.Energy()+top4.P())*(1-costh);
+      double weight_R = (top4.Energy()-top4.P())*(1+costh);
+      weight *= ((1+topPolarizationTarget)*weight_R+(1-topPolarizationTarget)*weight_L)/((1+topPolarizationReference)*weight_R+(1-topPolarizationReference)*weight_L);
+
+    }
+
+    return weight;
+
+};
---
+++ TopReweight.C
@@ -0,0 +1,169 @@
+#include "Stop_TopChi0_Reweighting.C" 
+
+#include "TStyle.h"
+#include "TRint.h"
+#include "TString.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1D.h"
+#include "TVector3.h"
+#include "TF1.h"
+#include "MuTree.h"
+
+bool IS_BATCH = false;
+double POL = -1.; // -1 ==> fully left-handed top; +1 ==> fully right-handed top
+int maxEventsUsed = -1; // If <0 ==> use all events in the file
+//int maxEventsUsed = 100000; // If <0 ==> use all events in the file
+
+using namespace std;
+using namespace ciemat;
+
+int main(int argc, char** argv){
+
+  // Set it to "true" if you do not want to see the histograms interactively
+  gROOT->SetBatch(IS_BATCH);
+  gStyle->SetOptStat(1111111);
+  gStyle->SetOptFit(1111);
+
+  // Open the input root file and set branches
+  // On pcciet3a, pcciet3b, pccmscie6
+  //TString sampleFile = "/data4/Fall11_WplusC_Trees_July2012/TTbar.root";
+  // Just for some checks (there are selection cuts applied on the following file)
+  //TString sampleFile = "/data4/Fall11_WplusC_Trees_July2012/TTbar_SSVHPNOMTNOISOreduced.root";
+  // Stop file
+//  TString sampleFile = "/data4/Fall11_WplusC_Trees_July2012/Stop.root";
+  TString sampleFile = "dcap://gaeds015.ciemat.es:22125/pnfs/ciemat.es/data/cms/store/user/delacruz/STop2012/NTuplesFeb2013/v1/merge_stops_signalmc_T2tt_Mstop-225to1200_mLSP-0to1000_Pythia_new.root";
+
+  Event ev;
+  Event* pointerToEvent = &ev;
+  printf("Processing sample '%s'...\n", sampleFile.Data());
+  TFile input_sample(sampleFile,"READONLY");
+  TTree* tree = 0;
+  input_sample.GetObject("MUTREE/MUTREE",tree);
+  if (!tree) input_sample.GetObject("MUTREE",tree);
+  tree->SetBranchAddress("event", &pointerToEvent);
+
+  int nentriesInTree = tree->GetEntriesFast();
+  if (maxEventsUsed<0) maxEventsUsed = nentriesInTree;
+  printf("\tThere are %d events in the file; running on %d events\n", nentriesInTree, maxEventsUsed);
+
+  TH1D* hCosb = new TH1D("hCosb", "cos(#theta_{tb})", 50, -1.0, 1.0);
+
+  printf("Input thetaEff for topPol %.3f is: %.3f\n", 1., GetThetaMixing(1., 950., 175., 425.));
+  printf("Input thetaEff for topPol %.3f is: %.3f\n", 0.5, GetThetaMixing(0.5, 950., 175., 425.));
+  printf("Input thetaEff for topPol %.3f is: %.3f\n", 0., GetThetaMixing(0., 950., 175., 425.));
+  printf("Input thetaEff for topPol %.3f is: %.3f\n", -0.5, GetThetaMixing(-0.5, 950., 175., 425.));
+  printf("Input thetaEff for topPol %.3f is: %.3f\n", -1., GetThetaMixing(-1., 950., 175., 425.));
+
+  // Event loop
+  for (int iEvent=0; iEvent<maxEventsUsed; iEvent++) {
+    if (tree->LoadTree(iEvent)<0) break;
+    tree->GetEntry(iEvent);
+
+    if (ev.genInfos.size()<=0) {
+      printf("This is not a MC file, EXIT!!!\n");
+      return -1;
+    }
+
+    if (iEvent%1000000==0) printf("... event index %d\n", iEvent);
+    
+    unsigned int ngen = ev.genParticles.size();
+
+    //double m_stop = 0.;
+    //double m_chi0 = 0.;
+    //double m_top = 0.;
+    std::vector<SUSYGenParticle> genParticles;
+    for (unsigned int ig=0; ig<ngen; ++ig) {
+      GenParticle gen = ev.genParticles[ig];
+      if (gen.status!=3) break;
+      SUSYGenParticle part;
+      part.pdgId = gen.pdgId;
+      part.energy = gen.energy;
+      part.pt = gen.pt;
+      part.eta = gen.eta;
+      part.phi = gen.phi;
+      part.firstMother = -1; if (gen.mothers.size()>0) part.firstMother = gen.mothers[0];
+      //if (abs(gen.pdgId)==1000006) m_stop = sqrt(pow(gen.energy,2)-pow(gen.pt*cosh(gen.eta),2));
+      //if (abs(gen.pdgId)==1000022) m_chi0 = sqrt(pow(gen.energy,2)-pow(gen.pt*cosh(gen.eta),2));
+      //if (abs(gen.pdgId)==6) m_top = sqrt(pow(gen.energy,2)-pow(gen.pt*cosh(gen.eta),2));
+
+      genParticles.push_back(part);
+    }
+    //printf("m_stop: %.3f, m_top: %.3f, m_chi0: %.3f\n", m_stop, m_top, m_chi0);
+
+    //double pol_new = POL;
+    //double pol_new = AverageTopPolarization_Stop_to_TopChi0(-1.1, genParticles);
+    //double weight = Reweight_Stop_to_TopChi0 (genParticles, 0., pol_new);
+
+    // m_stop=950 GeV, m_chi0=425 GeV, m_top=175 GeV
+    double thetaMixingTarget = -1.134; // Pol=-1
+    //double thetaMixingTarget = -0.437; // Pol=+1
+    double weight = Reweight_Stop_to_TopChi0_with_SUSYmodel (genParticles, 0.785, thetaMixingTarget);
+
+    for (unsigned int ig=0; ig<ngen; ++ig) {
+      GenParticle gen = ev.genParticles[ig];
+      if (gen.status!=3) break;
+      if (abs(gen.pdgId)!=5) continue;
+      if (gen.mothers.size()!=1) continue;
+      GenParticle genTop = ev.genParticles[gen.mothers[0]];
+      if (abs(genTop.pdgId)!=6) continue;
+
+      if (genTop.pdgId*gen.pdgId<0) continue;
+
+      double etop = genTop.energy;
+      double pxtop = genTop.pt*cos(genTop.phi);
+      double pytop = genTop.pt*sin(genTop.phi);
+      double pztop = genTop.pt*sinh(genTop.eta);
+      double ptop  = sqrt(pxtop*pxtop+pytop*pytop+pztop*pztop);
+      double mtop  = sqrt(etop*etop-ptop*ptop);
+      double pxb   = gen.pt*cos(gen.phi);
+      double pyb   = gen.pt*sin(gen.phi);
+      double pzb   = gen.pt*sinh(gen.eta);
+      double pb    = sqrt(pxb*pxb+pyb*pyb+pzb*pzb);
+
+      // We also need a stop
+      if (genTop.mothers.size()==0) continue;
+      GenParticle genStop = ev.genParticles[genTop.mothers[0]];
+      if (abs(genStop.pdgId)!=1000006) continue;
+
+      // Move top and fermion to the stop center-of-mass frame
+      TLorentzVector stop4;
+      stop4.SetPtEtaPhiE(genStop.pt, genStop.eta, genStop.phi, genStop.energy);
+      TVector3 betaS(-stop4.Px()/stop4.Energy(),-stop4.Py()/stop4.Energy(),-stop4.Pz()/stop4.Energy());
+      TLorentzVector topRef(pxtop,pytop,pztop,etop);
+      topRef.Boost(betaS); // keept this vector to calculate costh
+      TLorentzVector top4(pxtop,pytop,pztop,etop);
+      top4.Boost(betaS);
+      TLorentzVector b4(pxb,pyb,pzb,pb);
+      b4.Boost(betaS);
+
+      TVector3 betaV(-top4.Px()/top4.Energy(),-top4.Py()/top4.Energy(),-top4.Pz()/top4.Energy());
+      top4.Boost(betaV);
+      b4.Boost(betaV);
+
+      double costh = (topRef.Px()*b4.Px()+topRef.Py()*b4.Py()+topRef.Pz()*b4.Pz())/topRef.P()/b4.P();
+
+      hCosb->Fill(costh,weight);
+
+    }
+
+  }
+
+  // To see things interactively (if IS_BATCH == false);
+  TRint* app = new TRint("Wprime Analysis", &argc, argv);
+
+  hCosb->SetMinimum(0.);
+  hCosb->Draw();
+
+  // Fitting slope
+  TF1* f1 = new TF1("f1","[0]*(1+[1]*x)");
+  f1->SetParName(0,"ValueAt0");
+  f1->SetParName(1,"Slope");
+  hCosb->Fit(f1,"","same");
+
+  gROOT->GetListOfCanvases()->At(0)->SaveAs("costhb.jpg");
+
+  app->Run();
+
+  return 0;
+}
